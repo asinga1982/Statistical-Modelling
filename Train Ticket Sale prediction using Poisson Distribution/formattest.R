7cc419158c835f403165607f6f7eb8d61099a3f6
Aanish Singla <asingla1982@gmail.com>
2018-05-29 08:32:29 +0530

    Add files via upload

---
+++ Train Ticket Sale prediction using Poisson Distribution/formattest.R
@@ -0,0 +1,25 @@
+formattest <- function(File){
+  
+  ## Separate columns and format them
+  File1 <- File
+  File1$Datetime <- as.character(File1$Datetime)
+  File1 <- mutate(File1, tdate = substr(Datetime, 1,10), thr = substr(Datetime, 12,13))
+  ## convert Hour as factor
+  File1$thr <- as.factor(File1$thr)
+  ## FInd Weekday of the date
+  Dt <- as.Date("25-08-2012", "%d-%m-%Y")
+  File1 <- mutate(File1, weekday = weekdays(as.Date(File1$tdate, "%d-%m-%Y")), 
+                  age = -difftime(Dt, as.Date(File1$tdate, "%d-%m-%Y"), units = "weeks"),
+                  Mon = months(as.Date(File1$tdate, "%d-%m-%Y")),
+                  Day = substr(File1$tdate,1,2))
+  
+  File1$weekday <- as.factor(File1$weekday)
+  File1$Mon <- factor(File1$Mon)
+  File1$Day <- factor(as.character(File1$Day))
+  File1$age <- as.numeric(File1$age)
+  ## Remove Date coulmns
+  File1 <- select(File1, thr, weekday, age, Mon, Day)
+  
+  ## Return File1
+  File1 
+}
